df05e3ee59791851f31c557c7ef84b853cf11a9e
megakuul <megakuul@gmx.ch>
2023-03-12 22:15:34 +0100

    Added full functionality to the connectionTree and added comments

--- src/8Term/main.Designer.cs
+++ src/8Term/main.Designer.cs
@@ -1,7 +1,15 @@
-﻿namespace _8Term
+﻿using ConEmu.WinForms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace _8Term
 {
     partial class main
     {
+        /// <summary>
+        /// Height of AppBar
+        /// </summary>
+        private int _AppBarHeigt { get; set; }
+
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -20,6 +28,129 @@
             base.Dispose(disposing);
         }
 
+        /// <summary>
+        /// Gets Triggered on Resize of Window
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            resizeMainTabControlr();
+            if (this.WindowState == FormWindowState.Maximized)
+                resizeTerminals();
+        }
+
+        /// <summary>
+        /// Gets Triggered on ResizeEnd of Window
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnResizeEnd(EventArgs e)
+        {
+            base.OnResizeEnd(e);
+            resizeTerminals();
+        }
+
+        /// <summary>
+        /// This Resizes the ConEmu Consoles
+        /// Note that it is not resizing until you stop resizing.
+        /// This is done, because it uses a lot of GPU power if it dynamically resizes
+        /// </summary>
+        private void resizeTerminals()
+        {
+            if (ConsoleList != null)
+            {
+                foreach (ConEmuControl control in ConsoleList)
+                {
+                    control.Size = new Size(control.Parent.Width, control.Parent.Height);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Resizes the Tab Controlr
+        /// </summary>
+        private void resizeMainTabControlr()
+        {
+            mainTabControlr.Size = new Size(this.Size.Width, this.Size.Height - _AppBarHeigt);
+        }
+
+        /// <summary>
+        /// Design Initialization from custom Components
+        /// </summary>
+        private void InitializeCustomComponent()
+        {
+            _AppBarHeigt = connectionBx.Height + 50;
+            hostnameBx.Text = "Hostname";
+            hostnameBx.ForeColor = Color.Gray;
+            hostnameBx.Enter += HostnameBx_Enter;
+
+            foreach (Control obj in connectionInfoBx.Controls)
+            {
+                if (obj is TextBox || obj is NumericUpDown)
+                {
+                    obj.Leave += Obj_Leave;
+                }
+            }
+
+            connectionTree.AfterSelect += ConnectionTree_AfterSelect;
+        }
+
+        /// <summary>
+        /// After selection of a Node, write it to the Buffer and updateInformation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConnectionTree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            LastSelectedNode = e.Node;
+            updateInformation(e.Node);
+        }
+
+        /// <summary>
+        /// On Leave of a Information Element, changeConnection and updateInformation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Obj_Leave(object sender, EventArgs e)
+        {
+            Control control = sender as Control;
+
+            if (LastSelectedNode != null)
+            {
+                if (control != commandBx) {
+                    changeConnection(LastSelectedNode, hostnameBx.Text, usernameBx.Text, passwordBx.Text, pemKeyBx.Text, (int)portBx.Value);
+                } else {
+                    changeConnection(LastSelectedNode, hostnameBx.Text, usernameBx.Text, passwordBx.Text, pemKeyBx.Text, (int)portBx.Value, commandBx.Text);
+                }
+            }
+            updateInformation(LastSelectedNode);
+        }
+        /// <summary>
+        /// Display a hintText
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HostnameBx_Enter(object sender, EventArgs e)
+        {
+            if (hostnameBx.Text == "Hostname")
+            {
+                hostnameBx.Text = "";
+                hostnameBx.ForeColor = Color.Black;
+            }
+        }
+
+        /// <summary>
+        /// Opens the FileBrowser 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void selectBtn_Click(object sender, EventArgs e)
+        {
+            selectBrowser.ShowDialog();
+            pemKeyBx.Text = selectBrowser.FileName;
+        }
+
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -29,58 +160,34 @@
         private void InitializeComponent()
         {
             mainTabControlr = new TabControl();
-            tabPage1 = new TabPage();
-            conEmuControl1 = new ConEmu.WinForms.ConEmuControl();
-            tabPage2 = new TabPage();
             connectionTree = new TreeView();
             connectionBx = new GroupBox();
-            mainTabControlr.SuspendLayout();
-            tabPage1.SuspendLayout();
+            connectionInfoBx = new GroupBox();
+            commandBx = new TextBox();
+            selectBtn = new Button();
+            pemKeyBx = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            portBx = new NumericUpDown();
+            label3 = new Label();
+            portBxLbl = new Label();
+            passwordBx = new TextBox();
+            usernameBx = new TextBox();
+            hostnameBx = new TextBox();
+            selectBrowser = new OpenFileDialog();
             connectionBx.SuspendLayout();
+            connectionInfoBx.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)portBx).BeginInit();
             SuspendLayout();
             // 
             // mainTabControlr
             // 
-            mainTabControlr.Controls.Add(tabPage1);
-            mainTabControlr.Controls.Add(tabPage2);
             mainTabControlr.Dock = DockStyle.Top;
             mainTabControlr.Location = new Point(0, 0);
             mainTabControlr.Name = "mainTabControlr";
             mainTabControlr.SelectedIndex = 0;
             mainTabControlr.Size = new Size(1216, 357);
             mainTabControlr.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
-            tabPage1.Controls.Add(conEmuControl1);
-            tabPage1.Location = new Point(4, 24);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(1208, 329);
-            tabPage1.TabIndex = 0;
-            tabPage1.Text = "tabPage1";
-            tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // conEmuControl1
-            // 
-            conEmuControl1.AutoStartInfo = null;
-            conEmuControl1.IsStatusbarVisible = false;
-            conEmuControl1.Location = new Point(19, 6);
-            conEmuControl1.MinimumSize = new Size(1, 1);
-            conEmuControl1.Name = "conEmuControl1";
-            conEmuControl1.Size = new Size(763, 283);
-            conEmuControl1.TabIndex = 0;
-            conEmuControl1.Text = "conEmuControl1";
-            // 
-            // tabPage2
-            // 
-            tabPage2.Location = new Point(4, 24);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(1208, 329);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "tabPage2";
-            tabPage2.UseVisualStyleBackColor = true;
             // 
             // connectionTree
             // 
@@ -91,6 +198,7 @@
             // 
             // connectionBx
             // 
+            connectionBx.Controls.Add(connectionInfoBx);
             connectionBx.Controls.Add(connectionTree);
             connectionBx.Dock = DockStyle.Bottom;
             connectionBx.Location = new Point(0, 451);
@@ -100,6 +208,129 @@
             connectionBx.TabStop = false;
             connectionBx.Text = "Connections";
             // 
+            // connectionInfoBx
+            // 
+            connectionInfoBx.Controls.Add(commandBx);
+            connectionInfoBx.Controls.Add(selectBtn);
+            connectionInfoBx.Controls.Add(pemKeyBx);
+            connectionInfoBx.Controls.Add(label2);
+            connectionInfoBx.Controls.Add(label1);
+            connectionInfoBx.Controls.Add(portBx);
+            connectionInfoBx.Controls.Add(label3);
+            connectionInfoBx.Controls.Add(portBxLbl);
+            connectionInfoBx.Controls.Add(passwordBx);
+            connectionInfoBx.Controls.Add(usernameBx);
+            connectionInfoBx.Controls.Add(hostnameBx);
+            connectionInfoBx.Location = new Point(265, 12);
+            connectionInfoBx.Name = "connectionInfoBx";
+            connectionInfoBx.Size = new Size(263, 182);
+            connectionInfoBx.TabIndex = 3;
+            connectionInfoBx.TabStop = false;
+            connectionInfoBx.Text = "Information";
+            // 
+            // commandBx
+            // 
+            commandBx.Location = new Point(23, 153);
+            commandBx.MaxLength = 200;
+            commandBx.Name = "commandBx";
+            commandBx.Size = new Size(224, 23);
+            commandBx.TabIndex = 12;
+            // 
+            // selectBtn
+            // 
+            selectBtn.Location = new Point(221, 115);
+            selectBtn.Name = "selectBtn";
+            selectBtn.Size = new Size(26, 23);
+            selectBtn.TabIndex = 11;
+            selectBtn.Text = "...";
+            selectBtn.UseVisualStyleBackColor = true;
+            selectBtn.Click += selectBtn_Click;
+            // 
+            // pemKeyBx
+            // 
+            pemKeyBx.Location = new Point(103, 115);
+            pemKeyBx.MaxLength = 200;
+            pemKeyBx.Name = "pemKeyBx";
+            pemKeyBx.Size = new Size(112, 23);
+            pemKeyBx.TabIndex = 10;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(23, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 9;
+            label2.Text = "password";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 8;
+            label1.Text = "username";
+            // 
+            // portBx
+            // 
+            portBx.Location = new Point(209, 22);
+            portBx.Maximum = new decimal(new int[] { 65535, 0, 0, 0 });
+            portBx.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            portBx.Name = "portBx";
+            portBx.Size = new Size(38, 23);
+            portBx.TabIndex = 7;
+            portBx.Value = new decimal(new int[] { 22, 0, 0, 0 });
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(23, 118);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 15);
+            label3.TabIndex = 5;
+            label3.Text = "PEM Key";
+            // 
+            // portBxLbl
+            // 
+            portBxLbl.AutoSize = true;
+            portBxLbl.Location = new Point(174, 25);
+            portBxLbl.Name = "portBxLbl";
+            portBxLbl.Size = new Size(29, 15);
+            portBxLbl.TabIndex = 4;
+            portBxLbl.Text = "Port";
+            // 
+            // passwordBx
+            // 
+            passwordBx.Location = new Point(103, 86);
+            passwordBx.MaxLength = 100;
+            passwordBx.Name = "passwordBx";
+            passwordBx.PasswordChar = '*';
+            passwordBx.Size = new Size(144, 23);
+            passwordBx.TabIndex = 2;
+            // 
+            // usernameBx
+            // 
+            usernameBx.Location = new Point(103, 57);
+            usernameBx.MaxLength = 100;
+            usernameBx.Name = "usernameBx";
+            usernameBx.Size = new Size(144, 23);
+            usernameBx.TabIndex = 1;
+            // 
+            // hostnameBx
+            // 
+            hostnameBx.ForeColor = Color.Gray;
+            hostnameBx.Location = new Point(23, 22);
+            hostnameBx.MaxLength = 100;
+            hostnameBx.Name = "hostnameBx";
+            hostnameBx.Size = new Size(145, 23);
+            hostnameBx.TabIndex = 0;
+            hostnameBx.Text = "Hostname";
+            // 
+            // selectBrowser
+            // 
+            selectBrowser.Filter = "PEM-Dateien|*.pem";
+            // 
             // main
             // 
             ClientSize = new Size(1216, 651);
@@ -107,20 +338,33 @@
             Controls.Add(mainTabControlr);
             Name = "main";
             Load += main_Load;
-            mainTabControlr.ResumeLayout(false);
-            tabPage1.ResumeLayout(false);
             connectionBx.ResumeLayout(false);
+            connectionInfoBx.ResumeLayout(false);
+            connectionInfoBx.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)portBx).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private TabControl mainTabControlr;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
-        private ConEmu.WinForms.ConEmuControl conEmuControl1;
         private ToolStrip toolStipMain;
         private TreeView connectionTree;
         private GroupBox connectionBx;
+        private GroupBox connectionInfoBx;
+        private RichTextBox richTextBox1;
+        private Label label3;
+        private Label portBxLbl;
+        private Label label1;
+        private TextBox textBox3;
+        private TextBox usernameBx;
+        private TextBox hostnameBx;
+        private TextBox passwordBx;
+        private NumericUpDown portBx;
+        private TextBox commandBx;
+        private Button selectBtn;
+        private TextBox pemKeyBx;
+        private Label label2;
+        private OpenFileDialog selectBrowser;
     }
 }
--- src/8Term/main.cs
+++ src/8Term/main.cs
@@ -13,74 +13,46 @@ using _8Term.Properties;
 using _8Term.Config;
 using ConEmu.WinForms;
 using Newtonsoft.Json.Linq;
-using System.Runtime.InteropServices.JavaScript;
-using System.Reflection.Metadata.Ecma335;
 
 namespace _8Term
 {
     public partial class main : Form
     {
-        private int _AppBarHeigt { get; set; }
+        /// <summary>
+        /// Configuration variable
+        /// </summary>
+        private JObject _ConfigObj;
+
+        /// <summary>
+        /// Buffer for last selected Node
+        /// </summary>
+        public TreeNode LastSelectedNode;
+
+        /// <summary>
+        /// List of current active Consoles
+        /// </summary>
+        List<ConEmuControl> ConsoleList = new List<ConEmuControl>();
+
         public main()
         {
             InitializeComponent();
+            InitializeCustomComponent();
         }
-
-        List<ConEmuControl> ConsoleList = new List<ConEmuControl>();
 
         private void main_Load(object sender, EventArgs e)
         {
-            _AppBarHeigt = connectionBx.Height + 50;
-            loadConnections(JsonInterface.deserializeJsonConfig("conf.eterm"));
-            initConsole(mainTabControlr, "Hallo2", null);
+            _ConfigObj = JsonInterface.deserializeJsonConfig("conf.eterm");
+            loadConnections(_ConfigObj);
+            initConsole(mainTabControlr, "Taa", null);
         }
 
-        protected override void OnResize(EventArgs e)
-        {
-            base.OnResize(e);
-            resizeMainTabControlr();
-            if (this.WindowState == FormWindowState.Maximized)
-                resizeTerminals();
-        }
-
-        protected override void OnResizeEnd(EventArgs e)
-        {
-            base.OnResizeEnd(e);
-            resizeTerminals();
-        }
-
-        private void resizeTerminals()
-        {
-            if (ConsoleList != null)
-            {
-                foreach (ConEmuControl control in ConsoleList)
-                {
-                    control.Size = new Size(control.Parent.Width, control.Parent.Height);
-                }
-            }
-        }
-        private void resizeMainTabControlr()
-        {
-            mainTabControlr.Size = new Size(this.Size.Width, this.Size.Height - _AppBarHeigt);
-        }
-        private void loadConnections(JObject config)
-        {
-            connectionTree.Nodes.Clear();
-            JObject Object = (JObject)config["Connections"];
-
-            foreach (JProperty folder in Object.Properties())
-            {
-                var curNod = connectionTree.Nodes.Add((folder).Name);
-
-                foreach (JToken connection in folder.Values())
-                {
-                    curNod.Nodes.Add(((JProperty)connection).Name);
-                    //Attach Connection information to the Node with Node.Tag
-                }
-            }
-            
-        }
-
+        /// <summary>
+        /// Initializes a new Console (Tab)
+        /// </summary>
+        /// <param name="tabControl">Tabcontroller</param>
+        /// <param name="tabTitle">Tab Title</param>
+        /// <param name="startupCommand">Startup Command</param>
+        /// <returns></returns>
         private bool initConsole(TabControl tabControl, string tabTitle, string? startupCommand)
         {
             TabPage tabPage = new TabPage(tabTitle);
@@ -107,6 +79,127 @@ namespace _8Term
             });
 
             return true;
+        }
+
+        /// <summary>
+        /// Loads connection configuration to the connectionTree
+        /// </summary>
+        /// <param name="config">Configuration Variable</param>
+        private void loadConnections(JObject config)
+        {
+            connectionTree.Nodes.Clear();
+            JObject Object = (JObject)config["Connections"];
+
+            foreach (JProperty folder in Object.Properties())
+            {
+                var curFoldNod = connectionTree.Nodes.Add(folder.Name);
+
+                foreach (JToken connection in folder.Values())
+                {
+                    var curConNod = curFoldNod.Nodes.Add(((JProperty)connection).Name);
+                    JToken subCon = ((JProperty)connection).Value;
+                    curConNod.Tag = new {
+                        constr = subCon.SelectToken("constr") ?? "",
+                        hostname = subCon.SelectToken("hostname") ?? "",
+                        username = subCon.SelectToken("username") ?? "",
+                        password = subCon.SelectToken("password") ?? "",
+                        keyfile = subCon.SelectToken("keyfile") ?? "",
+                        port = subCon.SelectToken("port") ?? 22
+                    };
+                }
+            }
+        }
+
+        /// <summary>
+        /// The opposite of loadConnections, loads the configuration from the connectionTree
+        /// </summary>
+        /// <param name="config">Reference to Configuration Variable</param>
+        private void saveConnections(ref JObject config)
+        {
+            JObject tempFolders = new JObject();
+
+            foreach (TreeNode folderNod in connectionTree.Nodes)
+            {
+                JObject tempConnections = new JObject();
+                foreach (TreeNode conNod in folderNod.Nodes)
+                {
+                    JObject tempInformation = new JObject();
+
+                    dynamic tag = conNod.Tag;
+
+                    tempInformation.Add("constr", tag.constr);
+                    tempInformation.Add("hostname", tag.hostname);
+                    tempInformation.Add("username", tag.username);
+                    tempInformation.Add("keyfile", tag.keyfile);
+                    tempInformation.Add("port", tag.port);
+
+                    tempConnections.Add(conNod.Text, tempInformation);
+                }
+                tempFolders.Add(folderNod.Text, tempConnections);
+            }
+
+            config["Connections"] = tempFolders;
+        }
+
+        /// <summary>
+        /// Changes the connectionTree Tags of the passed Node.
+        /// This will also trigger saveConnections() and save the connectionTree to the Configuration Variable
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="hostname"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="keyfile"></param>
+        /// <param name="port"></param>
+        /// <param name="command"></param>
+        private void changeConnection(TreeNode node, string hostname, string username, string password, string keyfile, int port, string command=null)
+        {
+            string hostname_pref = hostname == "" || hostname == "Hostname" ? "" : $"-h {hostname} ";
+            string username_pref = username == "" ? "" : $"-u {username} ";
+            string password_pref = password == "" ? "" : $"-x {password} ";
+            string keyfile_pref = keyfile == "" ? "" : $"-k {keyfile} ";
+
+            string commandStr;
+            if (command == null)
+            {
+                commandStr = $"MSKConnect {hostname_pref}{username_pref}{password_pref}{keyfile_pref}-p {port}";
+            } else
+            {
+                commandStr = command;
+            }
+            node.Tag = new {
+                constr = commandStr,
+                hostname = hostname,
+                username = username,
+                password = password,
+                keyfile = keyfile,
+                port = port
+            };
+            saveConnections(ref _ConfigObj);
+        }
+
+        /// <summary>
+        /// Updates the Textboxes from the Information Panel
+        /// It takes the values from the Tag of the Node
+        /// </summary>
+        /// <param name="Node"></param>
+        private void updateInformation(TreeNode Node)
+        {
+            hostnameBx.Text = ((dynamic)Node?.Tag)?.hostname ?? "";
+
+            if (string.IsNullOrEmpty(hostnameBx.Text) || hostnameBx.Text == "Hostname")
+            {
+                hostnameBx.Text = "Hostname";
+                hostnameBx.ForeColor = Color.Gray;
+            } else {
+                hostnameBx.ForeColor = Color.Black;
+            }
+
+            usernameBx.Text = ((dynamic)Node?.Tag)?.username ?? "";
+            passwordBx.Text = ((dynamic)Node?.Tag)?.password ?? "";
+            pemKeyBx.Text = ((dynamic)Node?.Tag)?.keyfile ?? "";
+            commandBx.Text = ((dynamic)Node?.Tag)?.constr ?? "";
+            portBx.Value = Convert.ToDecimal(((dynamic)Node?.Tag)?.port ?? 22);
         }
     }
 }
